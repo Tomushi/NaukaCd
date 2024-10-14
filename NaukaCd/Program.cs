5539410a0c8dd91bb48f575f7cbcee021442b7e8
Tomasz Kuzel <tomek.kuzel@gmail.com>
2024-10-14 15:34:39 +0200

    Operatory

--- NaukaCd/Program.cs
+++ NaukaCd/Program.cs
@@ -7,12 +7,17 @@ namespace Operatory;
         static void Main(string[] args)
         {
             Console.WriteLine("Czesc Operatorzy");
+
             Console.WriteLine("Na dzis koniec jutro ciag dalszy");
 
             Console.WriteLine("Dzisiejsze zajecia byly bardzo ciekawe");
+        
             Console.WriteLine("Jak jest dzisiejsza data?");
+            
             Console.WriteLine("Dzisiaj jest :" + DateTime.Now.ToShortDateString());
+            
             Console.WriteLine("Wiec do zobaczenia jutro " + DateTime.Now + DateTime.Now.AddDays(1).ToShortDateString());
-
+            
+            Console.WriteLine("Czesc!")
         }
     }
