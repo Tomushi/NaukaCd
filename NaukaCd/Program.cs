0362a9692e3485d651048a228f163953cbf2e082
Tomasz Kuzel <tomek.kuzel@gmail.com>
2024-10-15 12:22:21 +0200

    zadanie 5 rowne 5

--- NaukaCd/Program.cs
+++ NaukaCd/Program.cs
@@ -4,17 +4,21 @@ namespace Operatory;
 
     class Program
     {
-        static void Main(string[] args)
+    static void Main(string[] args)
         {
             Console.WriteLine("Czesc Operatorzy");
 
-        int a = 5;
-        
-        int b = 8;
+            int a = 5;
 
-        int wynik = b % a;
+            int b = 8;
 
-        Console.WriteLine(wynik);
-
-    }
+            if (a == b)
+            {
+                Console.WriteLine("a jest rowne b");
+            }
+            else
+            {
+                Console.WriteLine("a nie jest rowne b");
+            }
+        }
     }
