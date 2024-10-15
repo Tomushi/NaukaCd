09cd5ef1e356675aa5b22eea8035d24c05a9c4ff
Tomasz Kuzel <tomek.kuzel@gmail.com>
2024-10-15 13:10:41 +0200

    Short; logical version

--- NaukaCd/Program.cs
+++ NaukaCd/Program.cs
@@ -5,21 +5,17 @@ namespace Operatory;
     class Program
     {
     static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj liczbę:");
+        int liczba = int.Parse(Console.ReadLine());
+
+        if (liczba % 2 == 0)
         {
-        Console.WriteLine("Czesc Operatorzy");
-        
-        Console.WriteLine("Podaj liczbe wieksza od 14 i mniejsza od 16");
-        
-        string liczba = Console.ReadLine();
-        
-        int a = int.Parse(liczba);
-
-        Console.WriteLine("Teraz wykonam skomplikowane obliczenia do ustalenia parzystosci kwantowej");
-        
-        if (a >= 14 && a <= 16)
-            {
-                Console.WriteLine(a + " to liczba nieparzysta");
-            }
-
+            Console.WriteLine(liczba + " jest liczbą parzystą.");
+        }
+        else
+        {
+            Console.WriteLine(liczba + " jest liczbą nieparzystą.");
         }
     }
+}
