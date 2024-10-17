c88e36904ac7c82812e50cb2af20b243643b58f3
Tomasz Kuzel <tomek.kuzel@gmail.com>
2024-10-17 20:20:58 +0200

    Liczby pierwsze

--- NaukaCd/Program.cs
+++ NaukaCd/Program.cs
@@ -2,54 +2,34 @@
 using System;
 using System.Data;
 
-namespace Operatory;
-class Program
+namespace Petle;
 
+class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Kalkulator");
+        int liczbaPierwszych = 0;
 
-        Console.WriteLine("Podaj pierwsza liczbe :");
-
-        int a = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("Podaj druga liczbe :");
-
-        int b = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("Teraz wybierz numer operacji.");
-
-        Console.WriteLine("1. Dodawanie");
-
-        Console.WriteLine("2. Odejmowanie");
-
-        Console.WriteLine("3. Mnozenie");
-
-        Console.WriteLine("4. Dzielenie");
-
-        int operacja = int.Parse(Console.ReadLine());
-
-        switch (operacja)
+        for (int liczba = 2; liczba <= 150; liczba++)
         {
-            case 1:
-                Console.WriteLine("Wynik to : " + (a + b));
-                break;
-            case 2:
-                Console.WriteLine("Wynik to : " + (a - b));
-                break;
-            case 3:
-                Console.WriteLine("Wynik to : " + (a * b));
-                break;
-            case 4:
-                Console.WriteLine("Wynik to : " + (a / b));
-                break;
-            default:
-                Console.WriteLine("Nie ma takiej operacji");
-                break;
-        }
-        
-        Console.WriteLine("Dzieki za wspolne liczenie :)");
+            bool czyPierwsza = true;
 
+            for (int i = 2; i <= Math.Sqrt(liczba); i++)
+            {
+                if (liczba % i == 0)
+                {
+                    czyPierwsza = false;
+                    break;
+                }
+            }
+
+            if (czyPierwsza)
+            {
+                liczbaPierwszych++;
+                Console.WriteLine(liczba + " jest liczbą pierwszą.");
+            }
+        }
+
+        Console.WriteLine("Liczba liczb pierwszych w zakresie 0 - 100: " + liczbaPierwszych);
     }
 }
