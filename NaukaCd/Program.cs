50b1a21fb296e8a04601b226b5551851a9a8a4e9
Tomasz Kuzel <tomek.kuzel@gmail.com>
2024-10-23 13:47:15 +0200

    Cwiczenia matematyczne z mozliwoscia kolejnych operacji.

--- NaukaCd/Program.cs
+++ NaukaCd/Program.cs
@@ -1,40 +1,84 @@
 ﻿using Microsoft.VisualBasic;
 using System;
+using System.ComponentModel.Design;
 using System.Data;
 
 namespace cwiczenia;
-
 class Program
-{
-    static void Main(string[] args)
     {
-        Console.WriteLine("Obliczamy pole i obwod prostokata");
-        Console.WriteLine("Podaj pierwszy bok:");
-        
-        string? input1 = Console.ReadLine();
-        int.TryParse(input1, out int bok1);
-        
-        Console.WriteLine("Podaj drugi bok:");
-        
-        string? input2 = Console.ReadLine();
-        int.TryParse(input2, out int bok2);
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Proste liczenia");
+            Console.WriteLine("Podaj pierwsza liczbe: ");
 
-        if(bok1 > 0 && bok2 > 0)
+            string? input1 = Console.ReadLine();
+            int.TryParse(input1, out int liczba1);
+
+            Console.WriteLine("Podaj druga liczbe: ");
+
+            string? input2 = Console.ReadLine();
+            int.TryParse(input2, out int liczba2);
+
+        while (true)
+        {
+            Console.WriteLine("Wybierz dzialanie: ");
+
+            Console.WriteLine("1. Dodawanie");
+
+            Console.WriteLine("2. Odejmowanie");
+
+            Console.WriteLine("3. Mnozenie");
+
+            Console.WriteLine("4. Dzielenie");
+
+            Console.WriteLine("5. Sprobuj inna operacje");
+
+            Console.WriteLine("6. Wyjscie");
+
+            string? dzialanie = Console.ReadLine();
+
+            int wynik = 0;
+
+            if (dzialanie == "1")
             {
-            int pole = bok1 * bok2;
-            Console.WriteLine($"Pole prostokata: {pole}");
+                wynik = liczba1 + liczba2;
+            }
+            else if (dzialanie == "2")
+            {
+                wynik = liczba1 - liczba2;
+            }
+            else if (dzialanie == "3")
+            {
+                wynik = liczba1 * liczba2;
+            }
+            else if (dzialanie == "4")
+            {
 
-            int obw = (bok1 + bok2) * 2;
-
-            Console.WriteLine($"Obwod prostokada wynosi: " + obw);
-
+                if (liczba2 != 0)
+                {
+                    wynik = liczba1 / liczba2;
+                }
+                else
+                {
+                    Console.WriteLine("Nie mozna dzielic przez 0");
+                    continue;
+                }
+            }
+            else if (dzialanie == "5")
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Niepoprawne dzialanie");
+                continue;
             }
 
-        else
-            {
-            Console.WriteLine("Liczba powinna byc dodatnia");
-            }
-                             
+
+            Console.WriteLine($"Wynik dzialania to: {wynik}");
+        
+        }
+        
     }
 
     
