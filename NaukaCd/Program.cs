7930eb47deb36e16c5faa1cb5680419f08ee66e3
Tomasz Kuzel <tomek.kuzel@gmail.com>
2024-10-28 16:57:03 +0100

    cwiczenia string

--- NaukaCd/Program.cs
+++ NaukaCd/Program.cs
@@ -4,81 +4,75 @@ using System.ComponentModel.Design;
 using System.Data;
 
 namespace cwiczenia;
+
 class Program
-    {
+{
     static void Main(string[] args)
+    {
+        Console.WriteLine("Cwiczenia");
+
+        Console.WriteLine("Podaj numer miesiaca: ");
+        int numerMiesiaca = int.Parse(Console.ReadLine());
+
+        string nazwaMiesiaca = "";
+
+        switch (numerMiesiaca)
         {
-            Console.WriteLine("Proste liczenia");
-            Console.WriteLine("Podaj pierwsza liczbe: ");
-
-            string? input1 = Console.ReadLine();
-            int.TryParse(input1, out int liczba1);
-
-            Console.WriteLine("Podaj druga liczbe: ");
-
-            string? input2 = Console.ReadLine();
-            int.TryParse(input2, out int liczba2);
-
-        while (true)
-        {
-            Console.WriteLine("Wybierz dzialanie: ");
-
-            Console.WriteLine("1. Dodawanie");
-
-            Console.WriteLine("2. Odejmowanie");
-
-            Console.WriteLine("3. Mnozenie");
-
-            Console.WriteLine("4. Dzielenie");           
-
-            Console.WriteLine("5. Wyjscie");
-
-            string? dzialanie = Console.ReadLine();
-                        
-            if (dzialanie == "1")
-            {
-                int wynik = liczba1 + liczba2;
-                Console.WriteLine($"Wynik dzialania to: {wynik}");
-            }
-            else if (dzialanie == "2")
-            {
-                int wynik = liczba1 - liczba2;
-                Console.WriteLine($"Wynik dzialania to: {wynik}");
-            }
-            else if (dzialanie == "3")
-            {
-                int wynik = liczba1 * liczba2;
-                Console.WriteLine($"Wynik dzialania to: {wynik}");
-            }
-            else if (dzialanie == "4")
-            {
-
-                if (liczba2 != 0)
-                {
-                    double wynik = (double) liczba1 / liczba2;
-                    Console.WriteLine($"Wynik dzialania to: {wynik}");
-                }
-                else
-                {
-                    Console.WriteLine("Nie mozna dzielic przez 0");
-                    continue;
-                }
-            }
-            else if (dzialanie == "5")
-            {
-                Console.WriteLine("Koniec programu");
+            case 1:
+                nazwaMiesiaca = "Styczen";
                 break;
 
-            }
-            else
-            {
-                Console.WriteLine("Niepoprawne dzialanie");
-                continue;
-            }
-                                            
-        }
-        
-    }
+            case 2:
+                nazwaMiesiaca = "Luty";
+                break;
 
+            case 3:
+                nazwaMiesiaca = "Marzec";
+                break;
+
+            case 4:
+                nazwaMiesiaca = "Kwiecien";
+                break;
+
+            case 5:
+                nazwaMiesiaca = "Maj";
+                break;
+
+            case 6:
+                nazwaMiesiaca = "Czerwiec";
+                break;
+
+            case 7:
+                nazwaMiesiaca = "Lipiec";
+                break;
+
+            case 8:
+                nazwaMiesiaca = "Sierpien";
+                break;
+
+            case 9:
+                nazwaMiesiaca = "Wrzesien";
+                break;
+
+            case 10:
+                nazwaMiesiaca = "Pazdziernik";
+                break;
+
+            case 11:
+                nazwaMiesiaca = "Listopad";
+                break;
+
+            case 12:
+                nazwaMiesiaca = "Grudzien";
+                break;
+
+            default:
+                nazwaMiesiaca = "Nie ma takkiego miesiaca";
+                break;
+
+        }
+
+        Console.WriteLine("Podany miesiac to: " + nazwaMiesiaca);
     
+    }
 }
