a0a20c15056d30a8d3fcf9569f07040d49e7f6ee
Tomasz Kuzel <tomek.kuzel@gmail.com>
2024-10-29 14:47:47 +0100

    Palindrom - char charArray Reverse

--- NaukaCd/Program.cs
+++ NaukaCd/Program.cs
@@ -9,71 +9,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Cwiczenia");
+        Console.WriteLine("Podaj wyraz a ja sprawdze czy jest to palindrom");
 
-        Console.WriteLine("Napisz nazwe miesiaca: ");
+        string? wyraz = Console.ReadLine();
+        
+        wyraz = wyraz?.Replace(" ", "").ToLower();
+        
+        char[] charArray = wyraz.ToCharArray();
 
-        string? miesiac = Console.ReadLine();
+        Array.Reverse(charArray);
 
-        int numerMiesiaca;
+        string odwroconyWyraz = new string(charArray);
 
-        switch (miesiac)
+        if (wyraz == odwroconyWyraz)
         {
-            case "Styczen":
-                numerMiesiaca = 1;
-                break;
-
-            case "Luty":
-                numerMiesiaca = 2;
-                break;
-
-            case "Marzec":
-                numerMiesiaca = 3;
-                break;
-
-            case "Kwiecien":
-                numerMiesiaca = 4;
-                break;
-
-            case "Maj":
-                numerMiesiaca = 5;
-                break;
-
-            case "Czerwiec":
-                numerMiesiaca = 6;
-                break;
-
-            case "Lipiec":
-                numerMiesiaca = 7;
-                break;
-
-            case "Sierpien":
-                numerMiesiaca = 8;
-                break;
-
-            case "Wrzesien":
-                numerMiesiaca = 9;
-                break;
-
-            case "Pazdziernik":
-                numerMiesiaca = 10;
-                break;
-
-            case "listopad":
-                numerMiesiaca = 11;
-                break;
-
-            case "Grudzien":
-                numerMiesiaca = 12;
-                break;
-
-            default:
-                Console.WriteLine ("Nie ma takiego miesiaca");
-                return;
-
+            Console.WriteLine("Masz Palindroma");
         }
-
-        Console.WriteLine($"Numer miesiaca to: " + numerMiesiaca);
-    
+        else
+        {
+            Console.WriteLine("To nie jest Palindrom");
+        }
     }
 }
