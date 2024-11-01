e2d16ac51d169117605202d416b30fdf8a54dfb1
Tomasz Kuzel <tomek.kuzel@gmail.com>
2024-11-01 17:04:07 +0100

    Kolejne cwiczenie z operatorami &&

--- NaukaCd/Program.cs
+++ NaukaCd/Program.cs
@@ -10,29 +10,50 @@ class Program
     static void Main(string[] args)
 
     {
-        Console.WriteLine("Podaj swoj wiek, sprawdze czy mozesz glosowac");
+        Console.WriteLine("Podaj trzy liczby calkowite.");
 
-        Console.WriteLine("Ile masz lat");
-               
-        int.TryParse(Console.ReadLine(), out int wiek);
-        
-        Console.WriteLine("Czy jestes obywatelem Polski? (tak/nie)");               
+        Console.WriteLine("Podaj pierwsza liczbe: ");
 
-        string odpowiedz = Console.ReadLine()?.ToLower();
+        int a = int.Parse(Console.ReadLine());
 
-        bool jestObywatelem = odpowiedz == "tak";
+        Console.WriteLine("Podaj druga liczbe: ");
 
-        if (wiek >= 18 && jestObywatelem)
-        {
-            Console.WriteLine("Mozesz glosowac");
-        }
-        else if (wiek < 18 || !jestObywatelem)
-        {
-            Console.WriteLine("Nie mozesz glosowac");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Podaj trzecia liczbe: ");
+
+        int c = int.Parse(Console.ReadLine());
+
+        if (a > 10 && b > 10 && c> 10)
+        { 
+            Console.WriteLine(" Wszystkie liczby sa wieksze od 10. Odpowiedz: TAK");
         }
         else
         {
-            Console.WriteLine("Niepoprawne dane");
+            Console.WriteLine("Wszystkie liczby sa wieksze od 10. Odpowiedz: NIE");
         }
+        if (a < 5 &&  b < 5 && c < 5)
+        {
+            Console.WriteLine("Jest liczba wieksza od 5.Odpowiedz: TAK");
+        }
+        else
+        {
+            Console.WriteLine("Jest liczba mniejsze od 5. Odpowiedz: NIE");
+        }
+        int licznikWiekszychOd20 = 0;
+
+        if (a > 20) licznikWiekszychOd20++;
+        if (b > 20) licznikWiekszychOd20++;
+        if (c > 20) licznikWiekszychOd20++;
+        if (licznikWiekszychOd20 == 1)
+
+        {
+            Console.WriteLine("Tylko jedna liczba jest wieksza od 20. Odpowiedz: TAK");
+        }
+        else
+        {
+            Console.WriteLine("Tylko jedna liczba jest wieksza od 20. Odpowiedz: NIE");
+        }
+
     }
 }
